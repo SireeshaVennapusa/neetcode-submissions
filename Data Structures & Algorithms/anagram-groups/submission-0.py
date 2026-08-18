de111675bbf090679514a99110d16b1f16c398e5
SireeshaVennapusa <142719426+SireeshaVennapusa@users.noreply.github.com>
2026-08-18 23:21:15 +0530

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.py
@@ -0,0 +1,9 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        grp={}
+        for word in strs:
+            key=''.join(sorted(word))
+            if key not in grp:
+                grp[key]=[]
+            grp[key].append(word)
+        return list(grp.values())            
