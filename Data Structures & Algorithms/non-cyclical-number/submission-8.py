66b0ef965259b946173df7683031566b7bdcb311
SireeshaVennapusa <142719426+SireeshaVennapusa@users.noreply.github.com>
2026-08-31 16:10:48 +0530

    Add: non-cyclical-number - submission-8

---
+++ Data Structures & Algorithms/non-cyclical-number/submission-8.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        lst=[]
+        tot=0
+        while n not in lst:
+            lst.append(n)
+            tot=0
+            while(n>0):
+                rem=n%10
+                tot=tot+(rem*rem)
+                n//=10
+            n=tot        
+            if tot==1:
+                return True
+        return False
+             
+            
+        
+                  
+         
+
+        
+                   
